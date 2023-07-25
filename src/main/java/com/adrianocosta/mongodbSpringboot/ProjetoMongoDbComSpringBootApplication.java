93b362904f860da1b18281e112408b5d3cc2e65a
adrianoadacosta <adrianoadacosta@outlook.com>
2023-07-25 20:22:14 -0300

    Projeto criado

---
+++ src/main/java/com/adrianocosta/mongodbSpringboot/ProjetoMongoDbComSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.adrianocosta.mongodbSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMongoDbComSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoMongoDbComSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adrianocosta/mongodbSpringboot/ProjetoMongoDbComSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrianocosta.mongodbSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoMongoDbComSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
